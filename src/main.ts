f83ee34d447cd4d4ac12aa5525792f02ea73a666
Ariel Davis <ariel.z.davis@icloud.com>
2022-06-11 00:43:04 -0700

    Start

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,183 @@
-console.log("hi there");
+import { Cheerio, load, type CheerioAPI, type Element } from "cheerio";
+import { readdir, readFile, writeFile } from "fs/promises";
+import fetch from "node-fetch";
+import path from "path";
+
+const root = "https://smlfamily.github.io/Basis";
+
+const DIR = "html";
+
+function assert(x: boolean) {
+  if (!x) {
+    throw new Error("assert failed");
+  }
+}
+
+function filterMap<T, U>(f: (x: T) => U | undefined, xs: T[]): U[] {
+  const ret = [];
+  for (const x of xs) {
+    const res = f(x);
+    if (res !== undefined) {
+      ret.push(res);
+    }
+  }
+  return ret;
+}
+
+function id<T>(x: T): T {
+  return x;
+}
+
+async function fetchAndWriteFiles(): Promise<File[]> {
+  const resp = await fetch(`${root}/manpages.html`);
+  const text = await resp.text();
+  const body = load(text);
+  const urls = filterMap(
+    id,
+    body("h4 a")
+      .toArray()
+      .map((x) => x.attribs["href"]),
+  );
+  return Promise.all(
+    urls.map(async (name) => {
+      const resp = await fetch(`${root}/${name}`);
+      const text = await resp.text();
+      await writeFile(path.join(DIR, name), text);
+      return { name, text };
+    }),
+  );
+}
+
+interface File {
+  name: string;
+  text: string;
+}
+
+interface InfoMap {
+  [k: string]: Info;
+}
+
+const MAX = 123123;
+function processFiles(files: File[]): InfoMap {
+  let i = 0;
+  const map: InfoMap = {};
+  for (const file of files) {
+    if (i >= MAX) {
+      break;
+    }
+    i++;
+    const info = getInfo(file.name, load(file.text));
+    const name = file.name.replace(/\.html$/, "");
+    assert(!(name in map));
+    map[name] = info;
+  }
+  return map;
+}
+
+interface Def {
+  items: string[];
+  desc: string;
+}
+
+interface Info {
+  synopsis: string | null;
+  synopsisDesc: string[];
+  interfaceDesc: string | null;
+  descriptionDefs: Def[];
+}
+
+function getCleanText(x: Cheerio<Element>): string {
+  return x.text().trim().replaceAll("\u00A0", " ").replaceAll(/\s+/g, " ");
+}
+
+function getInfo(name: string, $: CheerioAPI): Info {
+  const headers = $("h4").toArray();
+  const synopsisHeader = headers.find((x) => getCleanText($(x)) == "Synopsis");
+  const synopsisDesc: string[] = [];
+  let synopsis: string | null = null;
+  if (synopsisHeader === undefined) {
+    console.error(`${name}: missing synopsis`);
+  } else {
+    let cur = $(synopsisHeader).next();
+    assert(cur.length === 1 && cur.is("blockquote"));
+    synopsis = getCleanText(cur);
+    for (;;) {
+      cur = cur.next();
+      assert(cur.length === 1);
+      if (cur.is("p")) {
+        synopsisDesc.push(getCleanText(cur));
+      } else if (cur.is("hr")) {
+        break;
+      } else {
+        console.warn(`${name}: non-p non-hr in synopsis, ignoring`);
+      }
+    }
+  }
+  const interfaceHeader = headers.find(
+    (x) => getCleanText($(x)) == "Interface",
+  );
+  let interfaceDesc: string | null = null;
+  if (interfaceHeader === undefined) {
+    console.error(`${name}: missing interface`);
+  } else {
+    const elem = $(interfaceHeader).next();
+    assert(elem.length === 1 && elem.is("blockquote"));
+    interfaceDesc = getCleanText(elem);
+  }
+  const descriptionHeader = headers.find(
+    (x) => getCleanText($(x)) == "Description",
+  );
+  const descriptionDefs: Def[] = [];
+  if (descriptionHeader === undefined) {
+    console.error(`${name}: missing description`);
+  } else {
+    const descriptionDl = $(descriptionHeader).next();
+    assert(descriptionDl.length === 1 && descriptionDl.is("dl"));
+    let items: string[] = [];
+    for (const ch of descriptionDl.children().toArray()) {
+      const child = $(ch);
+      if (child.is("dt")) {
+        const t = getCleanText(child);
+        if (t.length !== 0) {
+          items.push(t);
+        }
+      } else if (child.is("dd")) {
+        descriptionDefs.push({ items, desc: getCleanText(child) });
+        items = [];
+      } else {
+        console.warn(`${name}: non-dt non-dd child in description, ignoring`);
+      }
+    }
+    assert(items.length === 0);
+  }
+  return {
+    synopsis,
+    synopsisDesc,
+    interfaceDesc,
+    descriptionDefs,
+  };
+}
+
+async function getFilesFromDir(): Promise<File[]> {
+  const fileNames = await readdir(DIR);
+  return Promise.all(
+    fileNames.map((name) =>
+      readFile(path.join(DIR, name)).then((text) => ({
+        name,
+        text: text.toString(),
+      })),
+    ),
+  );
+}
+
+async function main() {
+  // await fetchAndWriteFiles();
+  const files = await getFilesFromDir();
+  const map = processFiles(files);
+  await writeFile("out.json", JSON.stringify(map, null, 2));
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
